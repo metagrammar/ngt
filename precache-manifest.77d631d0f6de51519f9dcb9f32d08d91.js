26ac8b773f89867e2081de804f7c0949ff0b0bdb
Antonio Silva <antonioesilva@gmail.com>
2020-07-23 13:00:56 +0200

    Updates

---
+++ precache-manifest.77d631d0f6de51519f9dcb9f32d08d91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20a0cc32e444997b7195f5de255d045",
+    "url": "/metagrammar/deploy-ngt/index.html"
+  },
+  {
+    "revision": "b1004fc4e952fc3691f7",
+    "url": "/metagrammar/deploy-ngt/static/css/main.58591b1d.chunk.css"
+  },
+  {
+    "revision": "5e183165387ce7ed7b49",
+    "url": "/metagrammar/deploy-ngt/static/js/2.294df792.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/metagrammar/deploy-ngt/static/js/2.294df792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1004fc4e952fc3691f7",
+    "url": "/metagrammar/deploy-ngt/static/js/main.0d3891fd.chunk.js"
+  },
+  {
+    "revision": "5f0171428f934c4fe55a",
+    "url": "/metagrammar/deploy-ngt/static/js/runtime-main.17dc8e3b.js"
+  }
+]);
