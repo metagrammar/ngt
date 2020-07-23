eb7ece04e3146e468b0983fddc7efab481ee6b9c
Antonio Silva <antonioesilva@gmail.com>
2020-07-23 12:53:19 +0200

    deploying

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ firebase.initializeApp(firebaseConfig);
 
 const App = () => {
   // Set the states
-  const [ameliaBrown, setAmeliaBrown] = useState()
-  const [subfundOne, setSubfundOne] = useState()
+  const [ameliaBrown, setAmeliaBrown] = useState({})
+  const [subfundOne, setSubfundOne] = useState([])
 
 useEffect(() => {
   const fund_pac = firebase.database().ref().orderByChild('fund_id').equalTo('park_avenue_capital');
@@ -41,18 +41,37 @@ useEffect( () => {
 
   return (
     <>
+    <div className='header'>
+      <div className='header-cont contain'> NGT </div>
+    </div>
     <div className="App">
+      <div className='contain'>
         <div id='fundNameList'>
+
+          <div className='table-grid table-header table-columns'>
+            <div>Fund Name</div>
+            <div>Subfund Name</div>
+            <div>Share Class Name</div>
+            <div>Alerts</div>
+            <div>Report</div>
+          </div>
+
           {ameliaBrown && subfundOne &&
+            
             subfundOne.map(item => {
               return (
-                <div key={item.index}>
-                
-                 <p> Sub fund: {item.subfund_name}</p>
+                <div className='table-grid table-columns' key={item.index}>
+                  <div>{item.fund_name}</div>
+                  <div>{item.subfund_name}</div>
+                  <div>{item.share_class_name}</div>
+                  <div>{item.nb_alerts}</div>
+                  <div>{item.report_status}</div>
                 </div>
               )})
+
             }
         </div>
+      </div>
     </div>
     </>
   );
